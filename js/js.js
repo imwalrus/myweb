ad2e8a2c2d7ac2d7ddaf152792e7e4a2d17152c7
walrus <im_walrus@naver.com>
2020-12-23 09:28:01 +0900

    js

--- js/js.js
+++ js/js.js
@@ -1,24 +1,24 @@
-var name = document.getElementById("show").innerHTML;
+var name = document.getElementById('show').innerHTML;
 console.log(name);
-document.getElementById("show").innerHTML = "World";
+document.getElementById('show').innerHTML = 'World';
 
-var divTag = document.createElement("div");
-divTag.innerHTML = "Nice"; // <div>Nice</div>
+var divTag = document.createElement('div');
+divTag.innerHTML = 'Nice'; // <div>Nice</div>
 
-document.getElementById("show").append(divTag);
-console.log("---------------------------------------");
+document.getElementById('show').append(divTag);
+console.log('---------------------------------------');
 
-var li_1 = document.createElement("li");
-var li_2 = document.createElement("li");
-li_1.innerHTML = "Apple";
-li_2.innerHTML = "Orange";
+var li_1 = document.createElement('li');
+var li_2 = document.createElement('li');
+li_1.innerHTML = 'Apple';
+li_2.innerHTML = 'Orange';
 
-document.getElementById("fruit").append(li_1);
-document.getElementById("fruit").append(li_2);
+document.getElementById('fruit').append(li_1);
+document.getElementById('fruit').append(li_2);
 /////////////////////////////////////////
 
 var grade = 0;
 for (var i = 1; i <= 100; i++) {
     console.log(grade = grade + i);
 }
-document.write("결과값: " + grade + "입니다.")
+document.write('결과값: ' + grade + '입니다.')
--- js/list.js
+++ js/list.js
@@ -1,14 +1,18 @@
 function myFunc() {
-    document.write("<ul><li>Apple</li><li>Banana</li></ul>");
+    document.write('<ul><li>Apple</li><li>Banana</li></ul>');
 
+    document.write('<style> .even { background: red; }</style>');
+    document.write('<style> .odd { background: yellow; }</style>');
     let dan = 4;
-    document.write("<table border='1'>")
+    document.write('<ul>')
+    document.write('<table border="1">')
     for (let i = 1; i <= 10; i++) {
         if (i % 2 == 1) {
-            document.write('<tr><td class="even">' + dan + " X " + i + "</td>" + "<td> = </td><b><td>" + (dan * i) + "</b></td></tr>");
+            document.write('<tr><td class="even">' + dan + ' X ' + i + '</td>' + '<td> = </td><td>' + (dan * i) + '</td></tr>');
         } else {
-            document.write('<tr><td class="odd">' + dan + " X " + i + "</td>" + "<td> = </td><b><td>" + (dan * i) + "</b></td></tr>");
+            document.write('<tr><td class="odd">' + dan + ' X ' + i + '</td>' + '<td> = </td><td>' + (dan * i) + '</td></tr>');
         }
     }
-    document.write("</table>")
+    document.write('</table>')
+    document.write('</ul>')
 }
